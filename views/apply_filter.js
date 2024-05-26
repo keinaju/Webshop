9457a11e88b8e672ea3c5da3195512f84c6d2947
Jukka Keinanen <jukka.keinane@gmail.com>
2024-05-26 15:39:49 +0300

    Add select/unselect all categories

--- views/apply_filter.js
+++ views/apply_filter.js
@@ -7,10 +7,14 @@ function get_chosen_categories() {
     return chosen_categories.join(',');
 }
 
-function get_query_string(category_ids, page) {
-    return `?categories=${category_ids}&page=${page}`;
+function get_search_value() {
+    return document.querySelector('#search_node').value;
+}
+
+function get_query_string(search_string, category_ids, page) {
+    return `?search=${search_string}&categories=${category_ids}&page=${page}`;
 }
 
 function apply_filter() {    
-    location.href = 'http://localhost:3000/products' + get_query_string(get_chosen_categories(), 0);
+    location.href = 'http://localhost:3000/products' + get_query_string(get_search_value(), get_chosen_categories(), 0);
 }
---
+++ views/set_all_categories.js
@@ -0,0 +1,6 @@
+function set_all_categories(true_or_false) {
+    const checkbox_nodes = document.querySelectorAll('[name="categories"');
+    for (const node of checkbox_nodes) {
+        node.checked = true_or_false;
+    }
+}
