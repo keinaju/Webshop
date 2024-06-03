b50f59130e5a965d7662da913291852a9e3a15fd
Jukka Keinanen <jukka.keinane@gmail.com>
2024-06-03 13:04:24 +0300

    Remove redundant await

--- services/add_category_links.js
+++ services/add_category_links.js
@@ -2,6 +2,6 @@ const db = require('./db');
 
 module.exports = async function add_category_links(product_code, categories) {
     for (const category_id of categories) {
-        await db.query('CALL add_category_link(?, ?);', [product_code, category_id]);
+        db.query('CALL add_category_link(?, ?);', [product_code, category_id]);
     }
 };
