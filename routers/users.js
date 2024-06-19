ab2a1d4286cd2cf0e77336e07355ef186c52299c
Jukka Keinanen <jukka.keinane@gmail.com>
2024-06-19 10:54:41 +0300

    Add hash function

--- routers/users.js
+++ routers/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const add_user = require('../services/add_user');
 const multer = require('multer');
 const multer_parser = multer();
+const bcrypt = require('bcrypt');
 
 router.get('/users/add', (req, res) => {
     res.render('add_user');
@@ -12,8 +13,12 @@ router.post('/users/add', multer_parser.none(), async function (req, res, next) 
     const { email, password, first_name, last_name, role } = req.body;
 
     try {
-        await add_user(email, password, first_name, last_name, role);
-        res.send('User registered successfully.');
+        bcrypt.hash(password, 10, async (error, hashed_password) => {
+            if (error)
+                next(error);
+            await add_user(email, hashed_password, first_name, last_name, role);
+            res.send('User registered successfully.');
+        });
     }
     catch (error) {
         console.error('Error in user registration.', error.message);
