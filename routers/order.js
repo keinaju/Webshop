5faa6d7eae8332b9cb5d8295e6ef98cccfac7981
Jukka Keinanen <jukka.keinane@gmail.com>
2024-06-25 09:11:48 +0300

    Add error handling

--- routers/order.js
+++ routers/order.js
@@ -3,23 +3,32 @@ const router = express.Router();
 const get_user_by_email = require('../services/get_user_by_email');
 const pass = require('../services/pass');
 const get_product_by_id = require('../services/get_product_by_id');
+const multer = require('multer');
+const multer_parser = multer();
 
 router.get('/order', pass('customer', 'merchant', 'admin'), async (req, res) => {
-    const user_data = await get_user_by_email(req.user.email);
-    let shopping_cart = JSON.parse(user_data.shopping_cart);
-    let total_price = 0;
-    for (const product_in_cart of shopping_cart) {
-        total_price += product_in_cart.price_per_pc * product_in_cart.quantity;
-        const product_data = await get_product_by_id(product_in_cart.product_id);
-        product_in_cart.name = product_data.name;
-        product_in_cart.description = product_data.description;
-        product_in_cart.image_file = product_data.image_file;
+    try {
+        const user_data = await get_user_by_email(req.user.email);
+        let shopping_cart = JSON.parse(user_data.shopping_cart);
+        let total_price = 0;
+        for (const product_in_cart of shopping_cart) {
+            total_price += product_in_cart.price_per_pc * product_in_cart.quantity;
+            const product_data = await get_product_by_id(product_in_cart.product_id);
+            product_in_cart.name = product_data.name;
+            product_in_cart.description = product_data.description;
+            product_in_cart.image_file = product_data.image_file;
+        }
+        res.render('order', {
+            user: req.user,
+            products: shopping_cart,
+            products_as_json: user_data.shopping_cart,
+            total_price: total_price,
+        });
     }
-    res.render('order', {
-        user: req.user,
-        products: shopping_cart,
-        total_price: total_price,
-    });
+    catch (error) {
+        console.log('Error in GET /order process.', error.message);
+        next(error);
+    }
 });
 
 module.exports = router;
