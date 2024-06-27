33d7f5fbcafba7aab1d8cae6d293ae82c8e7edd1
Jukka Keinanen <jukka.keinane@gmail.com>
2024-06-27 12:22:03 +0300

    Add modify link

--- routers/product.js
+++ routers/product.js
@@ -3,9 +3,23 @@ const get_product_by_id = require('../services/get_product_by_id');
 const router = express.Router();
 
 router.get('/product', async (req, res, next) => {
+    let modify;
+    if (req.user) {
+        switch (req.user.role) {
+            case 'admin':
+                modify = true;
+                break;
+            case 'merchant':
+                modify = true;
+                break;
+            default:
+                modify = false;
+        }
+    }
     try {
         res.render('product_page', {
             user: req.user,
+            modify: modify,
             item: await get_product_by_id(req.query.code),
         });
     }
