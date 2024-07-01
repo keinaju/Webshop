a480bce6ea8fb5852025009f88f316cf212495a3
Jukka Keinanen <jukka.keinane@gmail.com>
2024-07-01 10:32:19 +0300

    Add validation to add_category

--- routers/categories.js
+++ routers/categories.js
@@ -3,25 +3,33 @@ const router = express.Router();
 const add_category = require('../services/add_category');
 const upload_file = require('../services/upload_file');
 const pass = require('../services/pass');
+const { body } = require('express-validator');
+const handle_validation_result = require('../services/handle_validation_result');
 
 router.get('/categories/add', pass('merchant', 'admin'), (req, res) => {
     res.render('add_category', { user: req.user });
 });
 
-router.post('/categories/add', pass('merchant', 'admin'), upload_file.single('image'), async function (req, res, next) {
-    let { category_name } = req.body;
+router.post('/categories/add',
+    pass('merchant', 'admin'),
+    upload_file.single('image'),
+    body('category_name').notEmpty().withMessage('Category name missing.'),
+    handle_validation_result,
+    async function (req, res, next) {
+        let { category_name } = req.body;
 
-    let filename = null;
-    if (req.file) filename = req.file.filename;
+        let filename = null;
+        if (req.file) filename = req.file.filename;
 
-    try {
-        await add_category(category_name, filename);
-        res.send('Category was uploaded successfully.');
+        try {
+            await add_category(category_name, filename);
+            res.send('Category was uploaded successfully.');
+        }
+        catch (error) {
+            console.error('Error in category addition.', error.message);
+            next(error);
+        }
     }
-    catch (error) {
-        console.error('Error in category addition.', error.message);
-        next(error);
-    }
-});
+);
 
 module.exports = router;
