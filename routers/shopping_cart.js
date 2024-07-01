bb854c37f5808c24c07fdabd65115a1b55f9ee19
Jukka Keinanen <jukka.keinane@gmail.com>
2024-07-01 09:02:11 +0300

    Add error handler

--- routers/shopping_cart.js
+++ routers/shopping_cart.js
@@ -7,15 +7,21 @@ const get_product_by_id = require('../services/get_product_by_id');
 const ShoppingCart = require('../types/shopping_cart');
 
 router.post('/shoppingcart/add', body_parser.json(), async (req, res, next) => {
-    if (!req.user) return res.json({ message: 'Please sign in before shopping.' });
-    if (!req.body.product_id || !req.body.quantity) throw new Error('Request doesn\'t have product data.');
-    //fetch user's existing cart from database
-    const { shopping_cart: cart_content } = await get_user_by_email(req.user.email);
-    const cart = new ShoppingCart(JSON.parse(cart_content) || []);
-    const { price } = await get_product_by_id(req.body.product_id);
-    cart.add_products(req.body.product_id, req.body.quantity, price);
-    await set_shopping_cart_by_email(req.user.email, cart.get_contents_json());
-    return res.json({ message: 'Product added to cart.\nCart now has ' + cart.get_short_info() });
+    try {
+        if (!req.user) return res.json({ message: 'Please sign in before shopping.' });
+        if (!req.body.product_id || !req.body.quantity) throw new Error('Request doesn\'t have product data.');
+        //fetch user's existing cart from database
+        const { shopping_cart: cart_content } = await get_user_by_email(req.user.email);
+        const cart = new ShoppingCart(JSON.parse(cart_content) || []);
+        const { price } = await get_product_by_id(req.body.product_id);
+        cart.add_products(req.body.product_id, req.body.quantity, price);
+        await set_shopping_cart_by_email(req.user.email, cart.get_contents_json());
+        return res.json({ message: 'Product added to cart.\nCart now has ' + cart.get_short_info() });
+    }
+    catch (error) {
+        console.log('Error while adding to shopping cart.', error.message);
+        next(error);
+    }
 });
 
 module.exports = router;
