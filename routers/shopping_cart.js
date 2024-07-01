e4f44c51ebfc3b650cc5394593804fd1d470436d
Jukka Keinanen <jukka.keinane@gmail.com>
2024-07-01 09:18:09 +0300

    Add shopping cart validation

--- routers/shopping_cart.js
+++ routers/shopping_cart.js
@@ -5,23 +5,35 @@ const get_user_by_email = require('../services/get_user_by_email');
 const set_shopping_cart_by_email = require('../services/set_shopping_cart_by_email');
 const get_product_by_id = require('../services/get_product_by_id');
 const ShoppingCart = require('../types/shopping_cart');
+const { body } = require('express-validator');
+const handle_validation_result = require('../services/handle_validation_result');
 
-router.post('/shoppingcart/add', body_parser.json(), async (req, res, next) => {
-    try {
-        if (!req.user) return res.json({ message: 'Please sign in before shopping.' });
-        if (!req.body.product_id || !req.body.quantity) throw new Error('Request doesn\'t have product data.');
-        //fetch user's existing cart from database
-        const { shopping_cart: cart_content } = await get_user_by_email(req.user.email);
-        const cart = new ShoppingCart(JSON.parse(cart_content) || []);
-        const { price } = await get_product_by_id(req.body.product_id);
-        cart.add_products(req.body.product_id, req.body.quantity, price);
-        await set_shopping_cart_by_email(req.user.email, cart.get_contents_json());
-        return res.json({ message: 'Product added to cart.\nCart now has ' + cart.get_short_info() });
+const validation_chain = [
+    body('product_id').notEmpty().withMessage('Product id missing.'),
+    body('quantity').isInt().withMessage('Invalid quantity.'),
+];
+
+router.post('/shoppingcart/add',
+    body_parser.json(),
+    validation_chain,
+    handle_validation_result,
+    async (req, res, next) => {
+        try {
+            if (!req.user) return res.json({ message: 'Please sign in before shopping.' });
+            if (!req.body.product_id || !req.body.quantity) throw new Error('Request doesn\'t have product data.');
+            //fetch user's existing cart from database
+            const { shopping_cart: cart_content } = await get_user_by_email(req.user.email);
+            const cart = new ShoppingCart(JSON.parse(cart_content) || []);
+            const { price } = await get_product_by_id(req.body.product_id);
+            cart.add_products(req.body.product_id, req.body.quantity, price);
+            await set_shopping_cart_by_email(req.user.email, cart.get_contents_json());
+            return res.json({ message: 'Product added to cart.\nCart now has ' + cart.get_short_info() });
+        }
+        catch (error) {
+            console.log('Error while adding to shopping cart.', error.message);
+            next(error);
+        }
     }
-    catch (error) {
-        console.log('Error while adding to shopping cart.', error.message);
-        next(error);
-    }
-});
+);
 
 module.exports = router;
