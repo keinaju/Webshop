7705aa6915ae5dcb82756421efe551a4da21466c
Jukka Keinanen <jukka.keinane@gmail.com>
2024-07-12 10:04:47 +0300

    Add nullish coalescing operator to port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT ?? 3000;
 const helmet = require('helmet');
 const session = require('express-session');
 const MemoryStore = require('memorystore')(session);
