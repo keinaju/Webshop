1e95d74f8e236062fb220a10592c0587c3b95a35
Jukka Keinanen <jukka.keinane@gmail.com>
2024-07-14 14:18:20 +0300

    Rename parameters

--- error_handler.js
+++ error_handler.js
@@ -1,6 +1,6 @@
-module.exports = function (err, req, res, next) {
-    const statusCode = err.statusCode || 500;
-    console.error(err.message, err.stack);
-    res.status(statusCode).json({ message: err.message });
+module.exports = function (error, request, response, next) {
+    const statusCode = error.statusCode || 500;
+    console.error(error.message, error.stack);
+    response.status(statusCode).json({ message: error.message });
     return;
 };
