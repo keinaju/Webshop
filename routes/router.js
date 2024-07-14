88947c7e61f5adbbc0bf4e6ae3cdd1ff1458dcc3
Jukka Keinanen <jukka.keinane@gmail.com>
2024-07-14 18:39:57 +0300

    Add line breaks

--- routes/router.js
+++ routes/router.js
@@ -2,26 +2,39 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', require('./default/get'));
+
 router.get('/catalog', require('./catalog/get'));
+
 router.get('/categories/add', require('./categories/get'));
 router.post('/categories/add', require('./categories/post'));
+
 router.get('/checkout', require('./checkout/get'));
 router.post('/checkout', require('./checkout/post'));
+
 router.get('/config', require('./config/get'));
 router.post('/config', require('./config/post'));
+
 router.get('/login', require('./login/get'));
 router.post('/login', require('./login/post'));
+
 router.post('/logout', require('./logout/post'));
+
 router.get('/logo', require('./logo/get'));
+
 router.get('/orders', require('./orders/get'));
 router.put('/orders', require('./orders/put'));
+
 router.get('/product/add', require('./product/add/get'));
 router.post('/product/add', require('./product/add/post'));
+
 router.get('/product/modify', require('./product/modify/get'));
 router.post('/product/modify', require('./product/modify/post'));
+
 router.get('/product/view', require('./product/view/get'));
+
 router.get('/register', require('./register/get'));
 router.post('/register', require('./register/post'));
+
 router.post('/shoppingcart/add', require('./shopping_cart/post'));
 
 module.exports = router;
