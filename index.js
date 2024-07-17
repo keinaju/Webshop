bcdfd484b56c9855b61d777e366cb1a3b1e81d21
Jukka Keinanen <jukka.keinane@gmail.com>
2024-07-17 12:18:03 +0300

    Use path module in express.static

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const helmet = require('helmet');
+const path = require('path');
 
 //View engine settings
 app.set('views', './views');
@@ -14,7 +15,7 @@ app.use(require('./session'));
 app.use(require('./routes/router'));
 
 //Serve static files from /assets and /public 
-app.use('/assets', express.static(process.cwd() + '/assets'));
+app.use('/assets', express.static(path.join(process.cwd(), 'assets')));
 app.use('/public', express.static(process.env.PUBLIC_DIRECTORY_PATH));
 
 app.use(require('./error_handler'));
