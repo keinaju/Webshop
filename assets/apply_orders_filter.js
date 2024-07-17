c86b62e4b1f85c63ff1a5800638cad0d196915b7
Jukka Keinanen <jukka.keinane@gmail.com>
2024-07-17 15:53:03 +0300

    Replace order status inline scripts with event listeners

--- assets/apply_orders_filter.js
+++ assets/apply_orders_filter.js
@@ -2,3 +2,8 @@ function apply_orders_filter() {
     const status = document.querySelector('#status_filter').value;
     location.href = `/orders?status=${status}&page=0`;
 }
+
+document.getElementById('status_filter').addEventListener('change', () => {
+    apply_orders_filter();
+    return false;
+});
--- assets/set_order_status.js
+++ assets/set_order_status.js
@@ -1,5 +1,5 @@
-async function set_order_status(order_id) {
-    const new_status = document.querySelector('#select_' + order_id).value;
+async function set_order_status(order_id, new_status) {
+    // const new_status = document.querySelector('#select_' + order_id).value;
     if (new_status == 'none') return;
     const requestOptions = {
         method: 'PUT',
@@ -13,4 +13,14 @@ async function set_order_status(order_id) {
     const data = await response.json();
     alert(data.message);
     location.reload(true);
+}
+
+const selects = document.querySelectorAll('.status_select');
+for (const select of selects) {
+    const order_id = select.getAttribute('order_id');
+    select.addEventListener('change', (event) => {
+        const new_status = event.currentTarget.value;
+        set_order_status(order_id, new_status);
+        return false;
+    });
 }
